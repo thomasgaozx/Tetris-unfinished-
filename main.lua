496ea2bad659fdec1d9fd54979185dbd0f3238ea
thomasgaozx <thomasgaozx@gmail.com>
2017-07-20 20:53:42 -0300

    Second commit

--- main.lua
+++ main.lua
@@ -6,65 +6,102 @@ function love.load()
     
 	--PLAYSPACE initialization
 	centerPoint={}
-    centerPoint.cpx=love.graphics.getWidth()/2
-    centerPoint.cpy=love.graphics.getHeight()/2
-    playSpace={width=6*unit, height=10*unit} -- PLAYSPACE SIZE
-    playSpace.leftBound=centerPoint.cpx-playSpace.width/2 -- x
-    playSpace.rightBound=centerPoint.cpx+playSpace.width/2 -- x
-    playSpace.upperBound=centerPoint.cpy-playSpace.height/2 --y
-    playSpace.ground=centerPoint.cpy+playSpace.height/2 -- y
+	centerPoint.cpx=love.graphics.getWidth()/2
+	centerPoint.cpy=love.graphics.getHeight()/2
+	playSpace={width=6*unit, height=10*unit} -- PLAYSPACE SIZE
+	playSpace.leftBound=centerPoint.cpx-playSpace.width/2 -- x
+	playSpace.rightBound=centerPoint.cpx+playSpace.width/2 -- x
+	playSpace.upperBound=centerPoint.cpy-playSpace.height/2 --y
+	playSpace.ground=centerPoint.cpy+playSpace.height/2 -- y
 
     --GRID initialization
-    grid={}
+	grid={}
 	for x = 0,(playSpace.width/unit-1) do
 		grid[x]={}
 		for y = 0,(playSpace.height/unit-1) do
-			grid[x][y] = {isFill = false}
+			grid[x][y] = false
 		end
 	end
 
     --TETROMINO initialization
-    newTetromino=tetromino.generateTetromino(3,1)
+	newTetromino=tetromino.generateTetromino(2,0)
 
     --GAME SETTING
-    incrementTime=0
+	incrementTime=0
+	incrementMove=0
+	legalMove=true
 
 end
 
 function love.keypressed(key)
-	if key == "down" then
-        tetrisBlock.y=tetrisBlock.y+1
-    end  
+	if key == "left" then
+		for i=1,4 do
+			block=newTetromino[i]
+			if ((block.x)+1)>(playSpace.width/unit-1) then
+				legalMove=false
+				break
+			elseif((block.x)+1)<=(playSpace.width/unit-1) then
+				if grid[block.x+1][block.y] then
+					legalMove=false
+					break
+				end
+			end
+		end
+		if legalMove then
+			tetromino.updateTetromino(newTetromino,1,0)
+		end
+		legalMove=true
+	end
 
-    if key == "left" then
-    	tetrisBlock.x=tetrisBlock.x-1
-    end
+	if key == "left" then
+		for i=1,4 do
+			block=newTetromino[i]
+			if ((block.x)-1)<0 then
+				legalMove=false
+				break
+			elseif((block.x)-1)>=0 then
+				if grid[block.x-1][block.y] then
+					legalMove=false
+					break
+				end
+			end
+		end
+		if legalMove then
+			tetromino.updateTetromino(newTetromino,-1,0)
+		end
+		legalMove=true
+	end
 
-    if key == "right" then
-    	tetrisBlock.x=tetrisBlock.x+1
-    end 
+	if key == "up" then
+		tetromino.rotate(newTetromino)
+	end
 end
 
 
 function love.update(dt)
+	if tetromino.reachEnd(newTetromino,grid,playSpace) then
+		newTetromino=tetromino.generateTetromino(2,0)
+	end
+
 	incrementTime=incrementTime+dt
+	incrementMove=incrementMove+dt
+	if love.keyboard.isDown('down') and incrementMove>=0.1 then
+		incrementMove=0
+		tetromino.updateTetromino(newTetromino,0,1)
+	end
+
 	if incrementTime>0.7 then
 		incrementTime=0
 		tetromino.updateTetromino(newTetromino,0,1)
-		if tetromino.reachEnd(newTetromino,grid,playSpace) then
-			newTetromino=generateTetromino(3,1)
-	    end
 	end
 end
 
 function love.draw()
-   
 	--draw PLAY SPACE
 	love.graphics.setColor(200,1250,200)
 	love.graphics.rectangle('fill',playSpace.leftBound, playSpace.upperBound, playSpace.width, playSpace.height)
-    
-    --draw TETRIS UNIT
-    love.graphics.setColor(0, 400, 0)
-    tetromino.drawTetromino(newTetromino, grid)
- 
+	
+	--draw TETRIS UNIT
+	love.graphics.setColor(0, 400, 0)
+	tetromino.drawTetromino(newTetromino, grid)
 end
--- tetromino.lua
+++ tetromino.lua
@@ -72,78 +72,80 @@ return {
         	block2.y=block1.y
         	block3.x=block2.x-1
         	block3.y=block2.y+1
-        	block4.x=block3.x-1
-        	block4.y=block3.y
-        end
+			block4.x=block3.x-1
+			block4.y=block3.y
+		end
 
-        if tType=='Z' then
-        	block1.x=x0
-        	block1.y=y0
-        	block2.x=block1.x-1
-        	block2.y=block1.y
-        	block3.x=block2.x+1
-        	block3.y=block2.y+1
-        	block4.x=block3.x+1
-        	block4.y=block3.y
-        end
-        return {block1,block2,block3,block4,tType}  --{block1,block2,block3,block4,tType} which is a tetromino object
-    end,
+		if tType=='Z' then
+			block1.x=x0
+			block1.y=y0
+			block2.x=block1.x-1
+			block2.y=block1.y
+			block3.x=block2.x+1
+			block3.y=block2.y+1
+			block4.x=block3.x+1
+			block4.y=block3.y
+		end
+		return {block1,block2,block3,block4,tType}  --{block1,block2,block3,block4,tType} which is a tetromino object
+	end,
 
-    updateTetromino=function(tetromino,xx,yy) --tetromino object={tType,block1,block2,block3,block4}; xx is delta x; yy is delta y
-        for i=1,4 do
-        	block=tetromino[i]
-        	block.x=block.x+xx
-        	block.y=block.y+yy
-        	tetromino[i]=block
-        end           
-    end,
+	updateTetromino=function(tetromino,xx,yy) --tetromino object={tType,block1,block2,block3,block4}; xx is delta x; yy is delta y
+		for i=1,4 do
+			block=tetromino[i]
+			block.x=block.x+xx
+			block.y=block.y+yy
+			tetromino[i]=block
+		end           
+	end,
 
-    rotate=function(tetromino) --tetromino object={tType,block1,block2,block3,block4}
-        if tetromino.tType=='O' then
-        	return tetromino
-        else
-        	centralBlock=tetromino[1]
-        	x0=centralBlock.x
-        	y0=centralBlock.y
-        	for i=2,4 do
-        		block=tetromino[i]
-        		xi=block.x
-        		yi=block.y
-        		block.x , block.y = x0-y0+yi , y0+x0-xi
-        		tetromino[i]=block
-        	end
-            return tetromino
-        end
-    end,
+	rotate=function(tetromino) --tetromino object={tType,block1,block2,block3,block4}
+		if tetromino.tType=='O' then
+			return tetromino
+		else
+			centralBlock=tetromino[1]
+			x0=centralBlock.x
+			y0=centralBlock.y
+			for i=2,4 do
+				block=tetromino[i]
+				xi=block.x
+				yi=block.y
+				block.x , block.y = x0-y0+yi , y0+x0-xi
+				tetromino[i]=block
+			end
+			return tetromino
+		end
+	end,
 
-    drawTetromino = function(tetromino,grid) --tetromino object={tType,block1,block2,block3,block4}; grid object
-        for i=1,4 do
-        	block=tetromino[i]
-            love.graphics.push()
-            love.graphics.translate(playSpace.leftBound, playSpace.upperBound)
-            love.graphics.draw(block.image, block.x*unit, block.y*unit)
-            love.graphics.pop()
-        end
-    end,
+	drawTetromino = function(tetromino,grid) --tetromino object={tType,block1,block2,block3,block4}; grid object
+		love.graphics.push()
+		love.graphics.translate(playSpace.leftBound, playSpace.upperBound)
+		for i=1,4 do
+			block=tetromino[i]
+			love.graphics.draw(block.image, block.x*unit, block.y*unit)
+		end
+		love.graphics.pop()
+	end,
 
-    reachEnd=function(tetromino,grid,playSpace) --tetromino object={tType,block1,block2,block3,block4}, grid object, playSpace object
-        unit=25
-        for i=1,4 do
-        	block=tetromino[i]
-        	if ((block.y)+1)>(playSpace.height/unit) then -- ERROR PRONE
-        		for i=1,4 do
-        			block=tetromino[i]
-        			grid[block.x][block.y].isFill=true
-        		end
-        		return true
-        	elseif (block.y+1)<(playSpace.height/unit) and grid[block.x][block.y+1].isFill then
-        		for i=1,4 do
-        			block=tetromino[i]
-        			grid[block.x][block.y].isFill=true
-        		end
-        		return true
-        	end
-        end
-        return false
-    end
+	reachEnd=function(tetromino,grid,playSpace) --tetromino object={tType,block1,block2,block3,block4}, grid object, playSpace object
+		unit=25
+		for i=1,4 do
+			block=tetromino[i]
+			if ((block.y)+1)>(playSpace.height/unit-1) then -- ERROR PRONE
+				for i=1,4 do
+					block=tetromino[i]
+					grid[block.x][block.y]=true
+				end
+				return true
+			elseif (block.y+1)<=(playSpace.height/unit-1) then
+				if grid[block.x][block.y+1] then
+					for i=1,4 do
+						block=tetromino[i]
+						grid[block.x][block.y]=true
+					end
+					return true
+				end
+			end
+		end
+		return false
+	end
 }
